e0c4bf818758a72e1631555de9831464e7718b34
nico.ortiz.a <nico.ortiz.a@gmail.com>
2018-11-19 14:30:19 -0500

    first commit

---
+++ testNGBatchFile.bat
@@ -0,0 +1,6 @@
+set projectLocation=C:\Users\BECARIOS_DITIC\workspace\TestNGBatchExample
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
+
