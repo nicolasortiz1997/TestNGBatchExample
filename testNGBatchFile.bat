6f934aede511d5ab0f3d25f3d42d6640994f347f
nico.ortiz.a <nico.ortiz.a@gmail.com>
2018-11-19 15:57:08 -0500

    testngBatchExample.sh .bin

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,6 +1,6 @@
 set projectLocation=/var/lib/jenkins/workspace/TestNGBatchExample
 cd %projectLocation%
-set classpath=%projectLocation%\lib\*
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
 pause
 
